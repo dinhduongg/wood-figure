6b06ba200b35a5f47afaed86ba732859ed85c75b
Duong <duongnd1402@gmail.com>
2023-07-09 22:02:00 +0700

    fix

--- frontend/axios/axios-client.ts
+++ frontend/axios/axios-client.ts
@@ -1,6 +1,4 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
-import { getSession } from 'next-auth/react'
 
 export const axiosPubllic = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -22,37 +20,3 @@ export const axiosPrivate = axios.create({
   },
   // withCredentials: true,
 })
-
-axiosPrivate.interceptors.request.use(
-  async (config) => {
-    const session = await getSession()
-    if (!config.headers['token']) {
-      config.headers['token'] = `Bearer ${session?.user.accessToken}`
-    }
-    return config
-  },
-  (error) => {
-    Promise.reject(error)
-  }
-)
-
-axiosPrivate.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const prevRequest = error?.config
-    if (error?.response?.status === 403 && !prevRequest?.sent) {
-      prevRequest.sent = true
-      const session = await getSession()
-
-      const res = await axiosPrivate.post('/auth/refresh-access', {
-        username: session?.user.username,
-      })
-
-      Cookies.set('access-token', res.data.accessToken)
-
-      prevRequest.headers['token'] = `Bearer ${res?.data.accessToken}`
-      return axiosPrivate(prevRequest)
-    }
-    return Promise.reject(error)
-  }
-)
